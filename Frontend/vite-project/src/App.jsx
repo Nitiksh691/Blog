ec1ecce48167d91fad8771c26cc4482046856a5e
Nitiksh <nitikshpal@gmail.com>
2025-06-03 14:29:23 +0530

    first commit

---
+++ Frontend/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline bg-amber-600">Welcome to the App</h1>
+    </div>
+  )
+}
+
+export default App
