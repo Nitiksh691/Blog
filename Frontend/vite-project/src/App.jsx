dc39b131d462c95f16c6c63a700342a0bcfa4019
Nitiksh <nitikshpal@gmail.com>
2025-06-03 14:51:56 +0530

    create pages and Route

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Signin from './Pages/Signin';
+import Signup from './Pages/Signup';
+import Dashboard from './Pages/Dashboard';
+import Project from './Pages/Project';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline bg-amber-600">Welcome to the App</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Project />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
